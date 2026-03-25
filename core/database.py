27a6a25d1db46ab8ff0b56417f94dcc9a5bef6d5
sirulab <chensiru.sjtu@gmail.com>
2026-03-25 20:10:39 +0800

    [fix] docker volumes, clean up unused files

--- core/database.py
+++ core/database.py
@@ -1,10 +1,15 @@
+# core/database.py
 from sqlmodel import SQLModel, create_engine, Session
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-sqlite_url = "sqlite:///database.db"
+# 確保 data 資料夾存在
+os.makedirs("data", exist_ok=True)
+
+# 將資料庫路徑改為 data/database.db
+sqlite_url = "sqlite:///data/database.db"
 engine = create_engine(sqlite_url, echo=False)
 
 def create_db_and_tables():
