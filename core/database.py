1389c8ee29cab96fce712c58f89980f42e89bf50
sirulab <chensiru.sjtu@gmail.com>
2026-03-29 12:59:29 +0800

    [fix] check before deployment

--- core/database.py
+++ core/database.py
@@ -1,15 +1,14 @@
-# core/database.py
-from sqlmodel import SQLModel, create_engine, Session
 import os
+from sqlmodel import SQLModel, create_engine, Session
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# 確保 data 資料夾存在
-os.makedirs("data", exist_ok=True)
+# 🔴 修改點：確保 data 資料夾存在，並將資料庫路徑改為 data/database.db
+DB_DIR = "data"
+os.makedirs(DB_DIR, exist_ok=True)
+sqlite_url = f"sqlite:///{DB_DIR}/database.db"
 
-# 將資料庫路徑改為 data/database.db
-sqlite_url = "sqlite:///data/database.db"
 engine = create_engine(sqlite_url, echo=False)
 
 def create_db_and_tables():
