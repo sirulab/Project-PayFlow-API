f942a6c9bdf8cfad81d961c60c2e5589064ef0a9
Siru <chensiru.sjtu@gmail.com>
2026-05-06 14:47:36 +0800

    [doc] clean notes

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ from features.products.router import router as product_router
 from features.orders.router import router as order_router
 from features.payments.router import router as payment_router
 
-# 使用最新的 Lifespan 管理啟動與關閉事件
+# Lifespan 管理啟動與關閉事件
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # 伺服器啟動時執行的動作 (Startup)
     create_db_and_tables()
     yield
-    # 伺服器關閉時執行的動作 (Shutdown) 可以寫在 yield 之後
 
-# 將 lifespan 綁定到 FastAPI 實例上
+# lifespan 綁定到 FastAPI 實例
 app = FastAPI(title="PayFlow API", lifespan=lifespan)
 
 # 註冊路由
